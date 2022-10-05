71ee4006c710b2ea41cab749ed1a0552f01def82
Yevhenii Veretennikov <yevhenii.veretennikov_nl@ntucguest.com>
2022-10-06 00:02:52 +0100

    add currency model

---
+++ Converter/DataLayer/Model/Currency.swift
@@ -0,0 +1,29 @@
+//
+//  Currency.swift
+//  Converter
+//
+//  Created by Yevhenii on 10/6/22.
+//
+
+import Foundation
+
+struct Currency {
+  let code: String
+  let name: String
+  
+  init(code: String, name: String) {
+    self.code = code
+    self.name = name
+  }
+  
+  init(code: String) {
+    self.code = code
+    self.name = Locale.current.localizedString(forCurrencyCode: code) ?? "NA"
+  }
+}
+
+extension Currency {
+  static let USD = Currency(code: "USD")
+  static let EUR = Currency(code: "EUR")
+  static let JPY = Currency(code: "JPY")
+}
